47b68152f165ba59c5238d6e4b5489c67104b4cd
casey@deccio.net <casey@deccio.net@7885c203-b7ea-840a-efb4-8f8d899814cf>
2014-08-25 20:49:41 +0000

    Move model fields into  their own module

---
+++ dnsvizwww/trunk/dnsvizwww/fields.py
@@ -0,0 +1,126 @@
+#
+# This file is a part of DNSViz, a tool suite for DNS/DNSSEC monitoring,
+# analysis, and visualization.
+# Author: Casey Deccio (casey@deccio.net)
+#
+# Copyright 2012-2014 Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+# rights in this software.
+# 
+# DNSViz is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# DNSViz is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+import dns.name
+
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class UnsignedSmallIntegerField(models.SmallIntegerField):
+    __metaclass__ = models.SubfieldBase
+    def to_python(self, value):
+        value = super(UnsignedSmallIntegerField, self).to_python(value)
+        if value is None:
+            return None
+        if value < 0:
+            value = 0x7FFF - value
+        return value
+
+    def get_prep_value(self, value):
+        value = super(UnsignedSmallIntegerField, self).get_prep_value(value)
+        if value is None:
+            return None
+        if value > 0x7FFF:
+            value = -(value - 0x7FFF)
+        return value
+
+class UnsignedIntegerField(models.IntegerField):
+    __metaclass__ = models.SubfieldBase
+    def to_python(self, value):
+        value = super(UnsignedIntegerField, self).to_python(value)
+        if value is None:
+            return None
+        if value < 0:
+            value = 0x7FFFFFFF - value
+        return value
+
+    def get_prep_value(self, value):
+        value = super(UnsignedIntegerField, self).get_prep_value(value)
+        if value is None:
+            return None
+        if value > 0x7FFFFFFF:
+            value = -(value - 0x7FFFFFFF)
+        return value
+
+class DomainNameField(models.CharField):
+    description = _("Domain name (with maximum length of %(max_length)s characters)")
+
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        self.canonicalize = kwargs.pop('canonicalize', True)
+        super(DomainNameField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if value is None:
+            return None
+        if isinstance(value, dns.name.Name):
+            name = value
+        else:
+            try:
+                name = dns.name.from_text(value)
+            except Exception, e:
+                raise ValidationError('%s: %s is of type %s' % (e, value, type(value)))
+        if self.canonicalize:
+            name = name.canonicalize()
+        return name
+
+    def get_prep_value(self, value):
+        if value is None:
+            return None
+        if isinstance(value, dns.name.Name):
+            name = value
+        else:
+            name = dns.name.from_text(value)
+        if self.canonicalize:
+            name = name.canonicalize()
+        return name.to_text()
+
+class BinaryField(models.Field):
+    #XXX no longer needed as of django 1.6
+    __metaclass__ = models.SubfieldBase
+
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] in ('django.db.backends.postgresql_psycopg2', 'django.db.backends.postgresql'):
+            return 'bytea'
+        elif connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
+            return 'blob'
+        elif connection.settings_dict['ENGINE'] == 'django.db.backends.sqlite3':
+            return 'BLOB'
+        raise Exception('Binary data type not known for %s db backend' % connection.settings_dict['ENGINE'])
+
+    def to_python(self, value):
+        if value is None:
+            return None
+        if isinstance(value, basestring):
+            return value
+        return str(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return None
+        if isinstance(value, bytearray):
+            return value
+        return bytearray(value)
