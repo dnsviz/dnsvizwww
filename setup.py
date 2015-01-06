1f181bea56cf70e686eaaaa10d227b8f01730b6b
Casey Deccio <casey@deccio.net>
2015-01-06 16:16:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ import sys
 from distutils.core import setup
 
 setup(name='dnsvizwww',
-        version='0.1.0',
+        version='0.2.0pre',
         author='Casey Deccio',
         author_email='casey@deccio.net',
-        url='https://code.google.com/p/dnsviz/',
+        url='https://github.com/dnsviz/dnsvizwww/',
         description='DNS analysis and visualization tool suite - Web and database components',
         long_description=open('README', 'r').read(),
-        license='COPYING',
+        license='LICENSE',
         packages=['dnsvizwww'],
         scripts=['bin/dnsget-db', 'bin/dnsviz-db', 'bin/dnsgrok-db'],
         data_files=[
-                ('share/doc/dnsvizwww', ['README', 'COPYING']),
+                ('share/doc/dnsvizwww', ['README', 'LICENSE']),
                 ('share/dnsvizwww/static/css', glob.glob(os.path.join('dnsvizwww', 'static', 'css', '*.css'))),
                 ('share/dnsvizwww/static/css/redmond', ['dnsvizwww/static/css/redmond/jquery-ui-1.10.4.custom.min.css']),
                 ('share/dnsvizwww/static/css/redmond/images', glob.glob(os.path.join('dnsvizwww', 'static', 'css', 'redmond', 'images', '*.png')) + glob.glob(os.path.join('dnsvizwww', 'static', 'css', 'redmond', 'images', '*.gif'))),
@@ -28,6 +28,7 @@ setup(name='dnsvizwww',
                 ('share/dnsvizwww/templates', glob.glob(os.path.join('dnsvizwww', 'templates', '*.html'))),
         ],
         requires=[
-                'dnsviz (==0.1.0)',
+                'dnsviz (==0.2.0)',
+                'django (==1.7.0)',
         ],
 )
