2cb942092a4330b5bc35de7606a13bebc8a1300f
Casey Deccio <casey@deccio.net>
2015-05-20 09:26:16 -0400

    Use args to create Form
    Use args to create Form rather than creating it on the fly from a
    function.

--- dnsvizwww/views.py
+++ dnsvizwww/views.py
@@ -665,7 +665,6 @@ def analyze(request, name, url_subdir=None):
     else:
         name_obj.retrieve_ancestry(name_obj.RDTYPES_DELEGATION)
         name_obj.retrieve_related(name_obj.RDTYPES_DELEGATION)
-    form_class = domain_analysis_form(name_obj.name)
 
     error_msg = None
     if request.POST:
@@ -673,7 +672,7 @@ def analyze(request, name, url_subdir=None):
         analysis_logger = log.IsolatedLogger(logging.DEBUG, request_logger, 'Error analyzing %s' % name_obj)
 
         # instantiate a bound form
-        analyze_form = form_class(request.POST)
+        analyze_form = DomainNameAnalysisForm(name_obj.name, request.POST)
         if analyze_form.is_valid():
             force_ancestor = analyze_form.cleaned_data['force_ancestor']
             extra_rdtypes = analyze_form.cleaned_data['extra_types']
@@ -714,7 +713,7 @@ def analyze(request, name, url_subdir=None):
 
     # instantiate an unbound form
     else:
-        analyze_form = form_class()
+        analyze_form = DomainNameAnalysisForm(name_obj.name)
 
     return render_to_response('analyze.html',
             { 'name_obj': name_obj, 'url_subdir': url_subdir, 'title': name_obj,
