c5326c61dbeedc32b2dc8d5db21ceaf7fab6cb29
Casey Deccio <casey@deccio.net>
2015-05-28 09:21:32 -0400

    Use humanize_name() to handle notices
    Use humanize_name() to handle notices, so unicode errors can be caught
    and handled appropriately.

--- dnsvizwww/notices.py
+++ dnsvizwww/notices.py
@@ -34,6 +34,7 @@ import re
 import dns.name
 
 from dnsviz.analysis import status as Status
+import dnsviz.format as fmt
 
 _rrset_node_re = re.compile(r'^(?P<name>[^|]+)\|(?P<rdtype>[A-Z]+)')
 _dnskey_node_re = re.compile(r'^(?P<name>[^|]+)\|(?P<alg>\d+)\|(?P<key_tag>\d+)')
@@ -83,7 +84,7 @@ def _get_label_for_node(notices, node_name, val):
         #TODO sort keys by dns name (i.e., not simply by textual name)
         if t1 == 'RRset':
             m2 = _rrset_node_re.search(m1.group('remnant'))
-            l = '%s/%s' % (dns.name.from_text(m2.group('name')).to_unicode(), m2.group('rdtype'))
+            l = '%s/%s' % (fmt.humanize_name(dns.name.from_text(m2.group('name')), True), m2.group('rdtype'))
             if m1.group('id') == '0':
                 l += ' (NXDOMAIN)'
             elif m1.group('id') == '1':
@@ -92,15 +93,15 @@ def _get_label_for_node(notices, node_name, val):
                 bisect.insort(notices['RRset status'][val[0]['status']],l)
         elif t1 == 'DNSKEY':
             m2 = _dnskey_node_re.search(m1.group('remnant'))
-            l = '%s/DNSKEY (alg %s, id %s)' % (dns.name.from_text(m2.group('name')).to_unicode(), m2.group('alg'), m2.group('key_tag'))
+            l = '%s/DNSKEY (alg %s, id %s)' % (fmt.humanize_name(dns.name.from_text(m2.group('name')), True), m2.group('alg'), m2.group('key_tag'))
             bisect.insort(notices['DNSKEY/DS/NSEC status'][val[0]['status']],l)
         elif t1 in ('DS','DLV'):
             m2 = _ds_node_re.search(m1.group('remnant'))
-            l = '%s/%s (alg %s, id %s)' % (dns.name.from_text(m2.group('name')).to_unicode(), t1, m2.group('alg'), m2.group('key_tag'))
+            l = '%s/%s (alg %s, id %s)' % (fmt.humanize_name(dns.name.from_text(m2.group('name')), True), t1, m2.group('alg'), m2.group('key_tag'))
             bisect.insort(notices['DNSKEY/DS/NSEC status'][val[0]['status']],l)
         elif t1.startswith('NSEC'):
             m2 = _nsec_node_re.search(m1.group('remnant'))
-            l = '%s proving non-existence of %s/%s' % (t1, dns.name.from_text(m2.group('name')).to_unicode(), m2.group('rdtype'))
+            l = '%s proving non-existence of %s/%s' % (t1, fmt.humanize_name(dns.name.from_text(m2.group('name')), True), m2.group('rdtype'))
             bisect.insort(notices['DNSKEY/DS/NSEC status'][val[0]['status']],l)
     else:
         m1 = _edge_re.search(node_name)
@@ -108,11 +109,11 @@ def _get_label_for_node(notices, node_name, val):
             t1 = m1.group('node_type')
             if t1 == 'del':
                 m2 = _del_re.search(m1.group('remnant'))
-                l = '%s to %s' % (dns.name.from_text(m2.group('parent')).to_unicode(), dns.name.from_text(m2.group('child')).to_unicode())
+                l = '%s to %s' % (fmt.humanize_name(dns.name.from_text(m2.group('parent')), True), fmt.humanize_name(dns.name.from_text(m2.group('child')), True))
                 bisect.insort(notices['delegation status'][val[0]['status']],l)
             elif t1 == 'digest':
                 m2 = _digest_re.search(m1.group('remnant'))
-                l = '%s/%s (alg %s, id %s)' % (dns.name.from_text(m2.group('name')).to_unicode(), m2.group('type'), m2.group('alg'), m2.group('key_tag'))
+                l = '%s/%s (alg %s, id %s)' % (fmt.humanize_name(dns.name.from_text(m2.group('name')), True), m2.group('type'), m2.group('alg'), m2.group('key_tag'))
             elif t1 == 'RRSIG':
                 m2 = _node_re.search(m1.group('remnant'))
                 m3 = _rrsig_dnskey_re.search(m2.group('remnant'))
@@ -120,22 +121,22 @@ def _get_label_for_node(notices, node_name, val):
                 t2 = m2.group('node_type')
                 if t2 == 'RRset':
                     m3 = _rrset_node_re.search(m2.group('remnant'))
-                    l = 'RRSIG %s/%s %s' % (dns.name.from_text(m3.group('name')).to_unicode(), m3.group('rdtype'), dnskey_str)
+                    l = 'RRSIG %s/%s %s' % (fmt.humanize_name(dns.name.from_text(m3.group('name')), True), m3.group('rdtype'), dnskey_str)
                 elif t2 == 'DNSKEY':
                     m3 = _dnskey_node_re.search(m2.group('remnant'))
-                    l = 'RRSIG %s/DNSKEY %s' % (dns.name.from_text(m3.group('name')).to_unicode(), dnskey_str)
+                    l = 'RRSIG %s/DNSKEY %s' % (fmt.humanize_name(dns.name.from_text(m3.group('name')), True), dnskey_str)
                 elif t2 in ('DS','DLV'):
                     m3 = _ds_node_re.search(m2.group('remnant'))
-                    l = 'RRSIG %s/%s %s' % (dns.name.from_text(m3.group('name')).to_unicode(), t2, dnskey_str)
+                    l = 'RRSIG %s/%s %s' % (fmt.humanize_name(dns.name.from_text(m3.group('name')), True), t2, dnskey_str)
                 elif t2.startswith('NSEC'):
                     m3 = _nsec_node_re.search(m2.group('remnant'))
-                    l = 'RRSIG %s proving non-existence of %s/%s %s' % (t2, dns.name.from_text(m3.group('name')).to_unicode(), m3.group('rdtype'), dnskey_str)
+                    l = 'RRSIG %s proving non-existence of %s/%s %s' % (t2, fmt.humanize_name(dns.name.from_text(m3.group('name')), True), m3.group('rdtype'), dnskey_str)
             elif t1.startswith('NSEC'):
                 m2 = _nsecc_re.search(m1.group('remnant'))
-                l = '%s proving non-existence of %s/%s' % (m2.group('type'), dns.name.from_text(m2.group('name')).to_unicode(), m2.group('rdtype'))
+                l = '%s proving non-existence of %s/%s' % (m2.group('type'), fmt.humanize_name(dns.name.from_text(m2.group('name')), True), m2.group('rdtype'))
             elif t1 == 'dname':
                 m2 = _dname_re.search(m1.group('remnant'))
-                l = 'CNAME synthesis of %s' % (dns.name.from_text(m2.group('name')).to_unicode())
+                l = 'CNAME synthesis of %s' % (fmt.humanize_name(dns.name.from_text(m2.group('name')), True))
     return l
 
 def _populate_notices(notices, obj, label=None):
